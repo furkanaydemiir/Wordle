7a7445c6c5ea582acd8e4547b43ccbf92ad8a8b0
Furkan Aydemir <sem_203@hotmail.com>
2026-04-24 01:09:11 +0300

    oto tab eklendi

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,29 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
-function Row({
-  handleChange,
-  handleBackSpace,
-  index,
-  onFocus,
-  disabled,
-  color,
-  value,
-}) {
+function Row({ handleChange, handleBackSpace, index, disabled, color, value }) {
+  const ref = useRef();
+ 
+   const tab = () =>{
+    ref.current.focus()
+   }
+   useEffect(()=>{
+    ref.current.focus()
+   },[handleChange])
+     
+
+
   return (
-    <>
-     <input
+    <input
+      ref={ref}
       value={value}
       disabled={disabled}
-      onKeyDown={handleBackSpace}
-      onChange={handleChange}
+      onKeyDown={(e) => handleBackSpace(e, index)}
+      onChange={(e) => handleChange(e, index)}
       type="text"
       maxLength={1}
       className={`uppercase w-14 h-14 border-2 text-2xl text-center text-white font-bold transition-all duration-700 ease-in-out transform 
         ${color} 
-        ${disabled && !color.includes('bg-') ? "opacity-40" : "opacity-100"}
-        ${color.includes('rotate-x-360') ? "[transform:rotateX(360deg)]" : ""}
-        focus:border-blue-400 outline-none rounded-sm`}/>
-    </>
+        ${disabled && !color.includes("bg-") ? "opacity-40" : "opacity-100"}
+        ${color.includes("rotate-x-360") ? "[transform:rotateX(360deg)]" : ""}
+        focus:border-blue-400 outline-none rounded-sm`}
+    />
   );
 }
 
